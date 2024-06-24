14eb1c660f9d786585dd3f22288fc723e128ef0c
gopalverma1303 <vgopaldps@gmail.com>
2024-06-24 15:39:12 +0530

    init the rss.ts script

---
+++ config/site.config.ts
@@ -0,0 +1,37 @@
+import type { Site } from "@/types";
+
+export const siteConfig: Site = {
+  name: "@bettercallgopal",
+  title: "Gopal Verma | @bettercallgopal | 🚀 Building something people want!",
+  description:
+    "I'm Gopal Verma, an indie developer, freelancer, and full-stack developer based in India. I do open-source projects and freelance work.",
+  keywords: [
+    "Gopal Verma",
+    "bettercallgopal",
+    "gopalverma",
+    "@bettercallgopal",
+    "gopalverma1303",
+    "github.com/GopalVerma1303",
+    "programmer",
+    "developer",
+    "engineer",
+    "founder",
+    "indie",
+    "designer",
+    "x.com/bettercallgopal",
+    "computer science",
+    "web developer",
+    "app developer",
+    "blockchina developer",
+  ],
+  siteUrl: "https://bettercallgopal.vercel.app",
+  creator: {
+    name: "Gopal Verma",
+    url: "https://bettercallgopal.vercel.app",
+  },
+  ogImage: "https://bettercallgopal.vercel.app/og.jpeg",
+  links: {
+    x: "https://x.com/bettercallgopal",
+    github: "https://github.com/GopalVerma1303",
+  },
+};
