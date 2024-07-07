cbda7f287167f77d71608019ccf3c64f209f1739
gopalverma1303 <vgopaldps@gmail.com>
2024-07-07 22:34:15 +0530

    added static site gen list

--- content/banks/web.ts
+++ content/banks/web.ts
@@ -71,3 +71,104 @@ export const web: Resource[] = [
     link: "https://www.smashingmagazine.com/",
   },
 ];
+
+export const DocsBlogsGen: Resource[] = [
+  {
+    title: "VuePress",
+    description:
+      "VuePress is a markdown-centered static site generator. You can write your content (documentations, blogs, etc.) in Markdown, then VuePress will help you to generate a static site to host them.",
+    tags: ["Documentation", "Blog", "VueJs"],
+    link: "https://vuepress.vuejs.org/",
+  },
+  {
+    title: "Nextra",
+    description:
+      "Nextra is a Next.js-based static site generator, ideal for creating documentation websites. It's simple to use and provides a clean, customizable interface.",
+    tags: ["Documentation", "NextJs", "Blogs"],
+    link: "https://nextra.site/",
+  },
+  {
+    title: "Docusaurus",
+    description:
+      "Docusaurus is a project for building, deploying, and maintaining open source project websites easily. It's created by Meta and works well with React-based projects.",
+    tags: ["Documentation", "Blog", "Meta"],
+    link: "https://docusaurus.io/",
+  },
+  {
+    title: "VitePress",
+    description:
+      "VitePress is a Static Site Generator (SSG) designed for building fast, content-centric websites. In a nutshell, VitePress takes your source content written in Markdown, applies a theme to it, and generates static HTML pages that can be easily deployed anywhere.",
+    tags: ["Documentation", "VueJs", "Vite"],
+    link: "https://vitepress.dev/",
+  },
+  {
+    title: "Gatsby",
+    description:
+      "Gatsby is a React-based open source framework for creating websites and apps. It's great for blogs, e-commerce sites, and documentation with its large plugin ecosystem.",
+    tags: ["Documentation", "Blog", "React", "GraphQL"],
+    link: "https://www.gatsbyjs.com/",
+  },
+  {
+    title: "Docz",
+    description:
+      "Docz enables you to quickly create live-reloading, seo-friendly, production-ready documentation sites with MDX and customize the look, feel and behavior when required by leveraging GatsbyJS and Gatsby theme shadowing.",
+    tags: ["Documentation", "Blog", "Gatsby", "Docz"],
+    link: "https://www.docz.site/",
+  },
+  {
+    title: "Kitdocs",
+    description:
+      "KitDocs makes it easy to build a documentation site with SvelteKit. You can think of it as a VitePress alternative for Sveltalowdas (i.e., the people of Svelte).",
+    tags: ["Documentation", "Blog", "Svelte", "SvelteKit"],
+    link: "https://kitdocs.vercel.app/",
+  },
+  {
+    title: "11ty",
+    description:
+      "A simpler static site generator. An alternative to Jekyll. Written in JavaScript. Transforms a directory of templates (of varying types) into HTML.",
+    tags: ["Documentation", "Blog", "JavaScript"],
+    link: "https://www.11ty.dev/",
+  },
+  {
+    title: "Mkdocs",
+    description:
+      "MkDocs is a fast, simple and downright gorgeous static site generator that's geared towards building project documentation.",
+    tags: ["Documentation", "Blog", "Mkdocs"],
+    link: "https://www.mkdocs.org/",
+  },
+  {
+    title: "Jekyllrb",
+    description:
+      "Jekyll is a static site generator. It takes text written in your favorite markup language and uses layouts to create a static website. You can tweak the site’s look and feel, URLs, the data displayed on the page, and more.",
+    tags: ["Documentation", "Blog", "Jekyllrb"],
+    link: "https://jekyllrb.com/",
+  },
+  {
+    title: "Starlight",
+    description:
+      "Everything you need to build a stellar documentation website. Fast, accessible, and easy-to-use.",
+    tags: ["Documentation", "Blog", "Astro"],
+    link: "https://starlight.astro.build/",
+  },
+  {
+    title: "Hugo",
+    description:
+      "Hugo is one of the most popular open-source static site generators. Written in Go, it's known for its speed and flexibility.",
+    tags: ["Documentation", "Blog", "Go"],
+    link: "https://gohugo.io/",
+  },
+  {
+    title: "mdBook",
+    description:
+      "mdBook is a command-line tool to create modern online books from Markdown files. It's used for Rust's official documentation and other technical documentation projects.",
+    tags: ["Documentation", "Rust", "Book"],
+    link: "https://rust-lang.github.io/mdBook/",
+  },
+  {
+    title: "GitBook",
+    description:
+      "GitBook is a free, open-source, static site generator that allows users to create and publish documentation, including books, internal wikis, and product and program documentation.",
+    tags: ["Documentation", "Book", "GitBook"],
+    link: "https://www.gitbook.com/",
+  },
+];
