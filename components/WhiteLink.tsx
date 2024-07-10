001b6e051a0aff07eb10ec83a09dbd639ea8373f
gopalverma1303 <vgopaldps@gmail.com>
2024-07-10 15:01:20 +0530

    feat: added /me and /stats

--- components/WhiteLink.tsx
+++ components/WhiteLink.tsx
@@ -15,7 +15,7 @@ const WhiteLink: React.FC<WhiteLinkProps> = ({
   return (
     <Link
       href={link}
-      className={`inline-flex justify-center items-center font-semibold ${
+      className={`inline-flex justify-center items-center mx-[4px] font-bold ${
         Icon ? "pb-1" : ""
       } rounded-none w-fit border-b border-white/30 hover:border-current transition-colors duration-500`}
     >
---
+++ content/me.ts
@@ -0,0 +1,51 @@
+export const WORKING: any[] = [
+  {
+    name: "GOPX.DEV",
+    link: "https://www.gopx.dev",
+    logo: "/icon-512x512.png",
+  },
+];
+
+export const BUILDING: any[] = [
+  {
+    name: "WebUI",
+    link: "https://github.com/GopalVerma1303/gopx-webui-kit",
+    logo: "/personal/me/webui-dark.png",
+  },
+  {
+    name: "MobUI",
+    link: "https://github.com/GopalVerma1303/gopx-mobui-kit",
+    logo: "/personal/me/mobui-dark.png",
+  },
+  {
+    name: "Dreams",
+    link: "https://github.com/GopalVerma1303/dreams",
+    logo: "/personal/me/dreams.png",
+  },
+];
+
+export const MAINTAINING: any[] = [
+  {
+    name: "WWW-CDS",
+    link: "https://github.com/CodeDeployingSquad/WWW",
+    logo: "https://www.codedeployingsquad.tech/assets/website/cdsglobe.png",
+  },
+  {
+    name: "Squadbook",
+    link: "https://github.com/codedeployingsquad/Squadbook",
+    logo: "/personal/me/SB.png",
+  },
+  {
+    name: "SquadChat",
+    link: "https://github.com/codedeployingsquad/SquadChat",
+    logo: "/personal/me/sc-light.png",
+  },
+];
+
+export const CORE_TEAM: any[] = [
+  {
+    name: "CDS",
+    link: "https://github.com/CodeDeployingSquad",
+    logo: "https://www.codedeployingsquad.tech/cds_circle.png",
+  },
+];
